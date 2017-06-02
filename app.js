4b9df2936946b3a65139350c661418e813f9a81e
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-02 11:36:15 +0000

    adding react, reactDom, and bootswatch theme

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(5000, () => {
+  console.log('App is listening on Port 5000');
+});
